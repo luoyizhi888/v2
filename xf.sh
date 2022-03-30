c96ea44592ca796b7652251095f7d4b6adfd8200
luoyizhi888 <228465483@qq.com>
2022-03-30 14:59:40 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "3d5382c1-7a0c-4e05-8e2d-036072d34248",
                         "alterId": 0
                     }
                 ],
